1f8777a1a732c3bf20b05930f9dd36932a1d8af4
6466761 <6466761@qq.com>
2024-05-13 15:27:12 +0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-我的Vim开发工具
+1: 修改Theme为catppuccin
+2: 增加lsp.coc-nvim插件
+3: 修改底部状态栏为heirline
+4: 增加java,rust,go,python,ts,vue...相关插件依赖
+5: 增加cmp-cmdline插件
+
+
+
+
--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,6 +7,27 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- { import = "astrocommunity.lsp.coc-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.utility.telescope-coc-nvim" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.typescript-deno" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.json" },
+  -- { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  -- { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   -- import/override with your plugins folder
 }
